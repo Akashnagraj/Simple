e6ad43013f9de0cd8440bbaf3519b02209a83bdb
Akashnagraj <akashnagraj77@gmail.com>
2024-08-29 12:44:41 +0530

    This is the First File
    I am adding here the first file and pushing it to git

---
+++ coding.dart
@@ -0,0 +1,39 @@
+abstract class Vehicle {
+  String? color;
+  double? maxSpeed;
+
+  void startEngine();
+  void accelerate();
+}
+
+class Car extends Vehicle {
+  void startEngine() {
+    print("Car is started");
+  }
+
+  void accelerate() {
+    print("Car is accelearted");
+  }
+}
+
+class Motorcycle extends Vehicle {
+  void startEngine() {
+    print("Motorcycle is started");
+  }
+
+  void accelerate() {
+    print("Motorcycle is accelearted");
+  }
+}
+
+void main() {
+  Vehicle obj1 = Car();
+  Vehicle obj2 = Motorcycle();
+  obj1.startEngine();
+  obj1.accelerate();
+
+  obj2.color = "Pink";
+  obj2.maxSpeed = 300;
+  print(obj1.color);
+  print(obj2.color);
+}
